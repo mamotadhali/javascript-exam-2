0a4e78184aa1aab64999ed27cecd12d3e8087361
momotadhali <mamotadhali1998@gmail.com>
2024-12-04 21:54:48 +0600

    first commit

---
+++ photos/p.js
@@ -0,0 +1,31 @@
+
+function loaddata(){
+    fetch('https://jsonplaceholder.typicode.com/photos')
+    .then(response => response.json())
+    .then(data => displaydata(data))
+    
+  }
+    loaddata();
+  const displaydata = (data) =>{
+    const coldata = document.getElementById("photos");
+        data.forEach((post) => {
+        
+          const postDiv = document.createElement("div");
+
+            postDiv.innerHTML = `
+            <div class="card" style="width: 10rem;">
+                <img src="img/Screenshot 2024-11-13 070739.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">${post.id}</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+                `;
+             coldata.appendChild(postDiv);         
+
+
+        });
+
+
+  }
---
+++ todos/t.js
@@ -0,0 +1,26 @@
+function loaddata(){
+  fetch('https://jsonplaceholder.typicode.com/todos')
+  .then(response => response.json())
+  .then(data => displaydata(data))
+  
+}
+  loaddata();
+const displaydata = (data) =>{
+  const coldata = document.getElementById("todos");
+      data.forEach((post) => {
+      
+        const postDiv = document.createElement("div");
+
+          postDiv.innerHTML = `
+          
+           <p> Mamota
+           </p>
+
+              `;
+           coldata.appendChild(postDiv);         
+
+
+      });
+
+
+}
